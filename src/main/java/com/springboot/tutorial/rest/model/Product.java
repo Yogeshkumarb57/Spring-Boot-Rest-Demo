16521e60e5227be2d61d48d5cfcbdafaaf4fecb7
Yogeshkumar <yogeshkumarb57@gmail.com>
2018-07-03 13:39:00 +0530

    added getters and setters in model

--- src/main/java/com/springboot/tutorial/rest/model/Product.java
+++ src/main/java/com/springboot/tutorial/rest/model/Product.java
@@ -19,4 +19,37 @@ public class Product {
 	private String productName;
 	private String productDescription;
 	private Double productPrice;
+	
+	
+	// adding getters, setters and toString, method. lombok was not working on ubuntu.
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public Double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productDescription="
+				+ productDescription + ", productPrice=" + productPrice + "]";
+	}
+	
 }
