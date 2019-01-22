4b094caa09f6a9e2467227e4135c12f4155a79bb
Yogeshkumar <yogeshkumarb57@gmail.com>
2019-01-22 22:18:11 +0530

    minor change

--- src/main/java/com/springboot/tutorial/rest/model/Product.java
+++ src/main/java/com/springboot/tutorial/rest/model/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -16,6 +17,7 @@ import lombok.Data;
 @ApiModel(description="Product model")
 @Entity
 @Data
+@XmlRootElement
 public class Product {
 
 	@Id
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into product
-values(1,'Mobile phone', 'Mobile', 10000);
+values(1,'Mobile', 'Mobile', 10000);
 
 insert into product 
-values(2,'Laptop Computer','Laptop', 50000);
+values(2,'Laptop','Laptop', 50000);
 
 insert into product
-values(3,'Mobile sim', 'Mobile sim', 100);
+values(3,'HDD', 'HDD', 3500);
